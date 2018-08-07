d259ea10948d50e088b4a24aa7af38add12bec4c
Edwin Siebel <edwin@yard.nl>
2018-08-07 08:23:49 +0000

    Add: description for links

--- src/Verordeningen/Metabox/MetaboxServiceProvider.php
+++ src/Verordeningen/Metabox/MetaboxServiceProvider.php
@@ -7,60 +7,60 @@ use OWC\PDC\Base\Foundation\ServiceProvider;
 class MetaboxServiceProvider extends ServiceProvider
 {
 
-	public function register()
-	{
-		$this->plugin->loader->addFilter('rwmb_meta_boxes', $this, 'registerMetaboxes', 10, 1);
-	}
+    public function register()
+    {
+        $this->plugin->loader->addFilter('rwmb_meta_boxes', $this, 'registerMetaboxes', 10, 1);
+    }
 
-	/**
-	 * register metaboxes.
-	 *
-	 * @param $metaboxes
-	 *
-	 * @return array
-	 */
-	public function registerMetaboxes($metaboxes)
-	{
+    /**
+     * register metaboxes.
+     *
+     * @param $metaboxes
+     *
+     * @return array
+     */
+    public function registerMetaboxes($metaboxes)
+    {
 
-		$prefix = '_pdc-verordening';
+        $prefix = '_pdc-verordening';
 
-		$metaboxes[] = [
-			'id'         => 'pdc-verordeningen',
-			'title'      => __('Verordening settings', 'pdc-verordeningen'),
-			'post_types' => ['pdc-verordeningen'],
-			'context'    => 'normal',
-			'priority'   => 'high',
-			'autosave'   => true,
-			'fields'     => [
-				[
-					'id'   => "{$prefix}-link",
-					'name' => __('Verordening link', 'pdc-verordeningen'),
-					'desc' => '',
-					'type' => 'text',
-				],
-				[
-					'id'   => "{$prefix}-new-link",
-					'name' => __('Verordening new link', 'pdc-verordeningen'),
-					'desc' => '',
-					'type' => 'text',
-				],
-				[
-					'id'         => "{$prefix}-active-date",
-					'name'       => esc_html__('Date new verordening active', 'pdc-verordeningen'),
-					'type'       => 'date',
-					'js_options' => [
-						'dateFormat'      => esc_html__('dd-mm-yy', 'pdc-verordeningen'),
-						'altFormat'       => 'yy-mm-dd',
-						'changeMonth'     => true,
-						'changeYear'      => true,
-						'showButtonPanel' => true,
-						'minDate'         => 0
-					],
-					'desc'       => esc_html__('(dd-mm-yy)', 'pdc-verordeningen'),
-				]
-			]
-		];
+        $metaboxes[] = [
+            'id' => 'pdc-verordeningen',
+            'title' => __('Verordening settings', 'pdc-verordeningen'),
+            'post_types' => ['pdc-verordeningen'],
+            'context' => 'normal',
+            'priority' => 'high',
+            'autosave' => true,
+            'fields' => [
+                [
+                    'id' => "{$prefix}-link",
+                    'name' => __('Verordening link', 'pdc-verordeningen'),
+                    'desc' => __('URL including http(s)://', 'pdc-verordeningen'),
+                    'type' => 'text',
+                ],
+                [
+                    'id' => "{$prefix}-new-link",
+                    'name' => __('Verordening new link', 'pdc-verordeningen'),
+                    'desc' => __('URL including http(s)://', 'pdc-verordeningen'),
+                    'type' => 'text',
+                ],
+                [
+                    'id' => "{$prefix}-active-date",
+                    'name' => esc_html__('Date new verordening active', 'pdc-verordeningen'),
+                    'type' => 'date',
+                    'js_options' => [
+                        'dateFormat' => esc_html__('dd-mm-yy', 'pdc-verordeningen'),
+                        'altFormat' => 'yy-mm-dd',
+                        'changeMonth' => true,
+                        'changeYear' => true,
+                        'showButtonPanel' => true,
+                        'minDate' => 0,
+                    ],
+                    'desc' => esc_html__('(dd-mm-yy)', 'pdc-verordeningen'),
+                ],
+            ],
+        ];
 
-		return $metaboxes;
-	}
+        return $metaboxes;
+    }
 }
