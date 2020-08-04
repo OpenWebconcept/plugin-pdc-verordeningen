0823003e8940b7374b243a368bedc0871f24e123
Mike van den Hoek <mvdhoek1991@gmail.com>
2020-08-04 16:31:03 +0200

    (chore): release v1.0.4

--- pdc-verordeningen.php
+++ pdc-verordeningen.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Plugin Name:       PDC Verordeningen
  * Plugin URI:        https://www.openwebconcept.nl
  * Description:       PDC Verordeningen
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Yard Internet
  * Author URI:        https://www.yardinternet.nl/
  * License:           GPL-3.0
